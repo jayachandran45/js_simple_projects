fb604a904e149202eb558b610a9ba3ca98992eb9
jayachandran45 <jayachandran.k11@gmail.com>
2023-07-31 18:19:47 +0530

    msg

--- vSwitch/app.js
+++ vSwitch/app.js
@@ -1,61 +1,26 @@
-// Function to switch between front and back cameras
+let currentCamera = "environment"; // 'environment' is used for the back camera, 'user' is used for the front camera
+
 async function switchCamera() {
+  const constraints = { video: { facingMode: currentCamera } };
+
   try {
-    // Enumerate available media devices
-    const devices = await navigator.mediaDevices.enumerateDevices();
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    const videoElement = document.querySelector("video");
 
-    // Filter video input devices (cameras)
-    const videoInputDevices = devices.filter(
-      (device) => device.kind === "videoinput"
-    );
-
-    if (videoInputDevices.length > 1) {
-      // Get the current media stream track
-      const videoTrack = stream.getVideoTracks()[0];
-
-      // Get the ID of the currently used camera
-      const currentDeviceId = videoTrack.getSettings().deviceId;
-
-      // Find the index of the currently used camera in the available video input devices
-      const currentIndex = videoInputDevices.findIndex(
-        (device) => device.deviceId === currentDeviceId
-      );
-
-      // Calculate the index of the next camera to switch to (cyclically)
-      const nextIndex = (currentIndex + 1) % videoInputDevices.length;
-
-      // Get the next camera's deviceId
-      const nextDeviceId = videoInputDevices[nextIndex].deviceId;
-
-      // Get the constraints for the new camera
-      const constraints = {
-        video: { deviceId: { exact: nextDeviceId } },
-        //audio: true, // You can include audio constraints if needed
-      };
-
-      // Get the new media stream with the switched camera
-      const newStream = await navigator.mediaDevices.getUserMedia(constraints);
-
-      // Replace the old video track with the new one
-      videoTrack.stop();
-      const newVideoTrack = newStream.getVideoTracks()[0];
-      stream.addTrack(newVideoTrack);
+    // Stop the existing tracks
+    if (videoElement.srcObject) {
+      const tracks = videoElement.srcObject.getTracks();
+      tracks.forEach((track) => track.stop());
     }
+
+    // Set the new stream to the video element
+    videoElement.srcObject = stream;
   } catch (error) {
-    console.error("Error switching camera:", error);
+    console.error("Error accessing media devices:", error);
   }
+
+  // Toggle between front and back cameras
+  currentCamera = currentCamera === "environment" ? "user" : "environment";
 }
 
-// Start capturing video stream
-let stream;
-navigator.mediaDevices
-  .getUserMedia({ video: true /* audio: true */ })
-  .then((mediaStream) => {
-    stream = mediaStream;
-    const videoElement = document.querySelector("video");
-    videoElement.srcObject = mediaStream;
-  })
-  .catch((error) => console.error("Error accessing media devices:", error));
-
-const iEle = document.querySelector("i");
-iEle.addEventListener("click", switchCamera);
+document.querySelector("i").addEventListener("click", switchCamera);
