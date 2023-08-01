5bd984f51fc2e07631c2526ada9257133d2abf65
jayachandran45 <jayachandran.k11@gmail.com>
2023-08-01 12:42:50 +0530

    msg

--- vSwitch/app.js
+++ vSwitch/app.js
@@ -1,16 +1,33 @@
 const videoEle = document.querySelector("video");
+let front = false;
+let mediaStream = null;
 
-let front = true;
-document.querySelector("button").onclick = () => {
+function switchCamera() {
   front = !front;
-};
-window.navigator.mediaDevices
-  .getUserMedia({
-    video: { facingMode: front ? "user" : "environment" },
-  })
-  .then((stream) => {
-    videoEle.srcObject = stream;
-  })
-  .catch((error) => {
-    console.error("Error accessing media devices:", error);
-  });
+  const facingMode = front ? "user" : "environment";
+
+  if (mediaStream) {
+    const tracks = currentStream.getTracks();
+    tracks.forEach((track) => track.stop());
+  }
+
+  // Request a new media stream with the updated facingMode
+  window.navigator.mediaDevices
+    .getUserMedia({
+      video: { facingMode: facingMode },
+    })
+    .then((stream) => {
+      mediaStream = stream;
+      videoEle.srcObject = stream;
+    })
+    .catch((error) => {
+      console.error("Error accessing media devices:", error);
+    });
+}
+
+// Add event listener to the button to toggle the camera
+const toggleCameraButton = document.querySelector("button");
+toggleCameraButton.addEventListener("click", switchCamera);
+
+// Initially, get the video stream with the default facing mode (front camera)
+switchCamera();
