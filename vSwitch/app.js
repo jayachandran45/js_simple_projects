46bf8c2314f3382949f4a2e35744aae033a5c1cf
jayachandran45 <jayachandran.k11@gmail.com>
2023-08-01 12:18:57 +0530

    msg

--- vSwitch/app.js
+++ vSwitch/app.js
@@ -1,18 +1,16 @@
 const videoEle = document.querySelector("video");
 
-function switchCamera() {
-  let front = false;
-  document.querySelector("button").onclick = () => {
-    front = !front;
-  };
-  window.navigator.mediaDevices
-    .getUserMedia({
-      video: { facingMode: front ? "user" : "environment" },
-    })
-    .then((stream) => {
-      videoEle.srcObject = stream;
-    })
-    .catch((error) => {
-      console.error("Error accessing media devices:", error);
-    });
-}
+let front = true;
+document.querySelector("button").onclick = () => {
+  front = !front;
+};
+window.navigator.mediaDevices
+  .getUserMedia({
+    video: { facingMode: front ? "user" : "environment" },
+  })
+  .then((stream) => {
+    videoEle.srcObject = stream;
+  })
+  .catch((error) => {
+    console.error("Error accessing media devices:", error);
+  });
