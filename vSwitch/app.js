f0e9c53592b93cc996c538ce674c547ba76eccd6
jayachandran45 <jayachandran.k11@gmail.com>
2023-08-01 12:01:50 +0530

    msg

--- vSwitch/app.js
+++ vSwitch/app.js
@@ -1,10 +1,24 @@
-const videoEle = document.querySelector("video");
+const videoEle = document.querySelector(video);
+let currentFacingMode = "environment"; // Initial facing mode, default to back camera
 
-window.navigator.mediaDevices
-  .getUserMedia({
-    video: { facingMode: { exact: "environment" } },
-  })
-  .then((resolver) => {
-    console.log(resolver);
-    videoEle.srcObject = resolver;
-  });
+function switchCamera() {
+  currentFacingMode = currentFacingMode === "user" ? "environment" : "user";
+
+  window.navigator.mediaDevices
+    .getUserMedia({
+      video: { facingMode: { exact: currentFacingMode } },
+    })
+    .then((stream) => {
+      videoEle.srcObject = stream;
+    })
+    .catch((error) => {
+      console.error("Error accessing media devices:", error);
+    });
+}
+
+// Add event listener to the button to toggle the camera
+const toggleCameraButton = document.getElementById("toggleCameraButton");
+toggleCameraButton.addEventListener("click", switchCamera);
+
+// Initially, get the video stream with the default facing mode (back camera)
+switchCamera();
